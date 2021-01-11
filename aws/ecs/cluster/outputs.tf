39fe47612ce9d2ba2454d1ef0e0d161f3d0b50e5
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-01-11 11:46:38 -0500

    Update outputs

--- aws/ecs/cluster/outputs.tf
+++ aws/ecs/cluster/outputs.tf
@@ -2,10 +2,14 @@ output "ecs_security_group" {
   value = aws_security_group.ecs.id
 }
 
-output "vpc_id" {
+output "ecs_vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "ecs_cluster" {
-  value = var.name
+output "ecs_subnet_ids" {
+  value = var.enable_public_instances ? module.vpc.public_subnets : module.vpc.private_subnets
+}
+
+output "ecs_cluster_arn" {
+  value = module.ecs.this_ecs_cluster_arn
 }
